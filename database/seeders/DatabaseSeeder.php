eeed9dbe3e0627a9b9fe7b137fc932346315bd58
nak-kx <vatanak135@gmail.com>
2025-07-03 17:00:33 +0700

    Resolve merge conflicts

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,9 +1,7 @@
 <?php
-
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +11,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Create 10 random users
+        User::factory()->count(10)->create();
 
+        // Create a specific user with fixed details
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'User',
+            'email' => 'user@gmail.com',
+            'password' => bcrypt('user1234'),
         ]);
     }
 }
